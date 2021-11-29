1109fec29817c408b8e863d1622335cb889b1e58
PickCool <89829496+PickCool@users.noreply.github.com>
2021-11-29 23:13:41 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace zd1
+namespace Pr4Ex1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Class1[] classes = new[]
-            {
-                new Class1(),
-                new Class2()
-            };
+            String string1, string2;
 
-            foreach (Class1 c in classes)
-                c.PrintAndDebug(322, 14.88d, 133.7f, 488.87m, new object(), "String");
+            Console.Write("Write the first word = "); string1 = Console.ReadLine();
+            Console.Write("Write the second word = "); string2 = Console.ReadLine();
+
+            string1 = new string(string1.Distinct().ToArray());
+            List<String> list2 = new List<string>();
+            for (int i = 0; i < string2.Length; i++)
+            {
+                list2.Add(string2[i].ToString());
+            }
+
+            for (int i = 0; i < string1.Length; i++)
+            {
+                Console.WriteLine(string1[i].ToString() + " = " + list2.Exists(symbol => symbol.ToString() == string1[i].ToString()));
+            }
 
             Console.ReadLine();
         }
     }
 }
+        
+
+    
+
